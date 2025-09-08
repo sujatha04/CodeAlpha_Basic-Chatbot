5a051b846f54b48b58d6b09555f866460c9b6859
sujatha04 <sujathathennarasu04@gmail.com>
2025-09-08 20:27:23 +0530

    Update Basic chatbot.py

--- Basic chatbot.py
+++ Basic chatbot.py
@@ -1,18 +1,18 @@
 def chatbot():
-    print("🤖 Chatbot: Hello! Type 'bye' to exit.")
+    print(" Chatbot: Hello! Type 'bye' to exit.")
     
     while True:
         user_input = input("You: ").lower().strip()
         
         if user_input == "hello":
-            print("🤖 Chatbot: Hi!")
+            print(" Chatbot: Hi!")
         elif user_input == "how are you":
-            print("🤖 Chatbot: I'm fine, thanks!")
+            print(" Chatbot: I'm fine, thanks!")
         elif user_input == "bye":
-            print("🤖 Chatbot: Goodbye! Have a nice day.")
+            print(" Chatbot: Goodbye! Have a nice day.")
             break
         else:
-            print("🤖 Chatbot: Sorry, I don't understand that.")
+            print(" Chatbot: Sorry, I don't understand that.")
 
 # Run chatbot
 chatbot()
